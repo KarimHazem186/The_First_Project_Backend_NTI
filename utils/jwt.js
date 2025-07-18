d08056e844d27c240ce958496b741383349c77ba
KarimHazem186 <karimhazem186@gmail.com>
2025-07-18 17:42:10 +0300

    finish utils

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,19 +1,21 @@
 const jwt =require('jsonwebtoken')
 
 
-function generateAccessToken(userId){
+function generateAccessToken(user){
     const payload={
-        userId
+        userId:user._id,
+        role:user.role
     };
     const secretKey = process.env.ACCESS_TOKEN_SECRET;
-    const options = {expiresIn:"15m"}
+    const options = {expiresIn:"1h"}
 
     return jwt.sign(payload,secretKey,options);
 }
 
-function generateRefreshToken(userId){
+function generateRefreshToken(user){
     const payload = {
-        userId
+        userId:user._id,
+        role:user.role
     };
     const secretKey = process.env.REFRESH_TOKEN_SECRET;
     const options = {expiresIn:"7d"}
