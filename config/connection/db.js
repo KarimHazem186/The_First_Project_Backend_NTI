d0af910590e112189098e2e049b71e2137c610bb
KarimHazem186 <karimhazem186@gmail.com>
2025-07-18 17:38:42 +0300

    finish config

--- config/connection/db.js
+++ config/connection/db.js
@@ -4,7 +4,7 @@ const connectDB = ()=>{
     mongoose
         .connect(process.env.MONGO_URL)
         .then(()=>console.log("✅ MongoDB connected"))
-        .catch((err)=>console.log("❌ DB connection error:",err))
+        .catch((err)=> console.log("❌ DB connection error:",err))
 };
 
 module.exports=connectDB
