6261f459d30aa31103b6ac53aba77473c1e7545b
KarimHazem186 <karimhazem186@gmail.com>
2025-07-18 17:48:18 +0300

    finish file server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // console.log('Backend app');
 const app = require('./app')
+const logger = require('./utils/loggers');
+
+
 const connectDB = require('./config/connection/db')
 
 const PORT = process.env.PORT
@@ -8,3 +11,7 @@ const PORT = process.env.PORT
 connectDB()
 
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
+
+// app.listen(PORT, () => {
+//   logger.info(`🚀 Server started on port ${PORT}`);
+// });
