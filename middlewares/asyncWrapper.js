fc8844928f9c5680c2bf05be24def8ee974e2554
KarimHazem186 <karimhazem186@gmail.com>
2025-07-23 14:45:01 +0300

    overwrite project

--- middlewares/asyncWrapper.js
+++ middlewares/asyncWrapper.js
@@ -1,7 +1,22 @@
-module.exports=(asyncFun)=>{
-    return(req,res,next)=>{
-        asyncFun(req,res,next).catch((err)=>{
-            next(err);
-        })
+// utils/asyncWrapper.js
+module.exports = (asyncFun) => {
+  return async (req, res) => {
+    try {
+      await asyncFun(req, res);
+    } catch (err) {
+      console.error("Caught error:", err);
+      res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || 'Internal Server Error',
+      });
     }
-}
+  };
+};
+
+// module.exports=(asyncFun)=>{
+//     return(req,res,next)=>{
+//         asyncFun(req,res,next).catch((err)=>{
+//             next(err);
+//         })
+//     }
+// }
